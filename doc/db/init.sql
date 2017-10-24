d64928338872786e4406a13e19975584274b5bc4
Administrator <Administrator@MSVTUWI1EVIZQBC>
2017-10-24 17:22:16 +0800

    test

--- doc/db/init.sql
+++ doc/db/init.sql
@@ -1,4 +1,4 @@
-# 创建数据库，并创建权限用户
+# 创建数据库，并创建权限和用户
 # CREATE DATABASE 'ssm' CHARACTER SET utf8;
 CREATE DATABASE ssm CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER 'ssm'@'%' IDENTIFIED BY 'ssm';
